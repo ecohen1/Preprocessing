c468e0a27fcbef31f33a040fc540f5dd79ee2176
Eli <elicohen2018@u.northwestern.edu>
2016-05-20 15:06:06 -0500

    tweaked other plot

--- csvpeakdetect.py
+++ csvpeakdetect.py
@@ -5,7 +5,7 @@ import matplotlib.pyplot as plt
 data = genfromtxt('moving-average-results.csv', delimiter=',')
 
 avgdata = [data[i][0] for i in range(len(data))]
-labeldata = [data[i][3]*.75 for i in range(len(data))]
+labeldata = [data[i][4]*.75 for i in range(len(data))]
 indices = [i for i in range(len(data))]
 # print avgdata
 # print labeldata
@@ -13,7 +13,7 @@ indices = [i for i in range(len(data))]
 plt.plot(indices, labeldata, 'g')
 plt.plot(indices, avgdata,'r')
 
-peaks = pd.peakdetect(avgdata, lookahead=50)
+peaks = pd.peakdetect(avgdata, lookahead=25)
 peakvals = [peaks[i] for i in range(len(peaks))]
 peakvalsx = [val[0] for val in peakvals[0]]
 peakvalsy = [val[1] for val in peakvals[0]]
